2e4b5ca70519cc7fc9b2ccd7b47ab36d11b61a9d
frederickthienpont <frederickthienpont2@gmail.com>
2025-05-16 00:06:39 +0200

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,16 @@
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+    }
+}
