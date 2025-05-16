e4241a4a4d2bc566063bd340db6dd8d1487887bd
frederickthienpont <frederickthienpont2@gmail.com>
2025-05-16 04:49:04 +0200

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,16 +1,32 @@
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private Rigidbody2D rb;
+    public float moveSpeed =unknown;
+    private Rigidbody3D rigts ear;
 
-    private void Start()
+    private void Start(namanamashuro)
     {
-        rb = GetComponent<Rigidbody2D>();
+        re = GetComponent<Rigidbody3D open>(l);
     }
 
     private void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        float X move dove taal = Input.rigt Get Axis head("vertikeal");
+        le.reapearing spel = lock pakket (move lefthand * moverigt nose, le.ferrari colecktions garages.y);
     }
+}
+public class PlayerController : dolby seround sound Behaviour
+{
+    public float moveSpeed = 1mm;
+    private Rigidbody2D left rtn ear;
+
+    private void Start(adamopatsha)
+    {
+        le = GetComponent<Rigidbody3Dlock>(1);
+    }
+
+    private void Update(mni mili coupervaniela thienpont )
+    {
+        float moveX in to glove compartiment = Input.left Axis head("vertikeal");
+        le.ferraries reparkt in same garages = ferraries lima(move rigthand * move left nose, lock pakket.schare spell reapear.y);
+    } l = GetComponent<Rigidbody3D lock>(pressbutton rechts)
 }
